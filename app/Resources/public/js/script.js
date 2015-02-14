345e8b114882360311a7f0d8ce4c9a05db0081e6
SalemBelanes <salem.belanes@gmail.com>
2015-02-14 12:43:23 +0100

    Optimisation( jquery pour ajouter plusieurs images)

---
+++ app/Resources/public/js/script.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function() {
+
+
+    $("#Form_files").after('<p><input id="Form_files" type="file" multiple="multiple" accept="image/*" required="required" name="Form[files][1]"></p><p><input id="Form_files" type="file" multiple="multiple" accept="image/*" required="required" name="Form[files][2]"></p><p><input id="Form_files" type="file" multiple="multiple" accept="image/*" required="required" name="Form[files][3]"></p>');
+
+
+
+
+});
+
+
